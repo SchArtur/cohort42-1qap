612ee93b7a58ced913521f9e1051f6f8055394ee
larysa7408 <larisa7408tema@gmail.com>
2024-08-29 18:59:45 +0200

    commit

--- Selenium_First_Project/src/test/java/homework/HomeWork13.java
+++ Selenium_First_Project/src/test/java/homework/HomeWork13.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class HomeWork13 extends BaseHomeWork {
+public class HomeWork13 extends BaseHomeWork{
     @Test
     @DisplayName("Проверка успешной авторизации пользователя")
-    void test1() {
-        fillInputField(By.cssSelector("[name='Email']"), "larysa@gmail.com");
-        fillInputField(By.cssSelector("[name='Password']"), "qwerty");
+    void test1(){
+        checkElementOnDisplay(By.cssSelector("[href='/login']"));
+       // clickOnElement(By.cssSelector("[href='/login']"));
+        fillInputField(By.cssSelector("[id='Email']"), "larysa@gmail.com");
+        fillInputField(By.cssSelector("[id='Password']"), "qwerty");
         clickOnElement(By.cssSelector("[value='Log in']"));
         checkElementOnDisplay(By.cssSelector("[href='/logout']"));
     }
