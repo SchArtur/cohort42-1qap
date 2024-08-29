1c3cddb9cf4fb1a3b06fcf7bdb65cfcdb19b0ded
larysa7408 <larisa7408tema@gmail.com>
2024-08-29 23:46:05 +0200

    created a test method for authorization on the page, using the inclusion of By.CssSelector.

--- Selenium_First_Project/src/test/java/homework/HomeWork13.java
+++ Selenium_First_Project/src/test/java/homework/HomeWork13.java
@@ -10,10 +10,9 @@ public class HomeWork13 extends BaseHomeWork{
     @Test
     @DisplayName("Проверка успешной авторизации пользователя")
     void test1(){
-        checkElementOnDisplay(By.cssSelector("[href='/login']"));
-       // clickOnElement(By.cssSelector("[href='/login']"));
-        fillInputField(By.cssSelector("[id='Email']"), "larysa@gmail.com");
-        fillInputField(By.cssSelector("[id='Password']"), "qwerty");
+
+        fillInputField(By.cssSelector("[name='Email']"),"larysa@gmail.com");
+        fillInputField(By.cssSelector("[name='Password']"),"qwerty");
         clickOnElement(By.cssSelector("[value='Log in']"));
         checkElementOnDisplay(By.cssSelector("[href='/logout']"));
     }
