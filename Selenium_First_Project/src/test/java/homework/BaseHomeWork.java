41d11f0db38ec4197f3cfafa46f36bce650bf13b
larysa7408 <larisa7408tema@gmail.com>
2024-08-29 23:46:05 +0200

    created a test method for authorization on the page, using the inclusion of By.CssSelector.

--- Selenium_First_Project/src/test/java/homework/BaseHomeWork.java
+++ Selenium_First_Project/src/test/java/homework/BaseHomeWork.java
@@ -22,33 +22,34 @@ public class BaseHomeWork {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-search-engine-choice-screen");
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://demowebshop.tricentis.com/login");
         Assertions.assertEquals("Demo Web Shop. Login", driver.getTitle(), "Открыта страница с другим Title");
     }
+    public WebElement getElement(By locator){
+        return wait.until(ExpectedConditions.visibilityOf(driver.findElement(locator)));
+    }
     public WebElement waitForClickableElement(By locator) {
         return wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
-
     public Alert getAlert() {
         return wait.until(ExpectedConditions.alertIsPresent());
     }
-
     public void fillInputField(By locator, String value) {
         WebElement element = waitForClickableElement(locator);
         element.click();
         element.clear();
         element.sendKeys(value);
-        Assertions.assertEquals(value, element.getAttribute("value"),
-                "Введенные данные отличаются от данных у элемента");
+        Assertions.assertEquals(value, element.getAttribute("value"), "Введенные данные отличаются от данных у элемента");
     }
     public void clickOnElement(By locator) {
         waitForClickableElement(locator).click();
     }
     public void checkElementOnDisplay(By locator) {
-        Assertions.assertTrue(driver.findElements(locator).size() > 0,
-                String.format("Элемент с локатором - %s не найден", locator));
+        Assertions.assertTrue(driver.findElements(locator).size() > 0,String.format("Элемент с локатором - %s не найден", locator));
     }
     @AfterEach
     public  void closeDriver(){
