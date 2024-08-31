ba1c6de2ce1fca15bfa97595cf7eadfae927c715
larysa7408 <larisa7408tema@gmail.com>
2024-08-31 16:46:47 +0200

    logic was corrected and click on Log out was added

--- Selenium_First_Project/src/test/java/homework/HomeWork13.java
+++ Selenium_First_Project/src/test/java/homework/HomeWork13.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class HomeWork13 extends BaseHomeWork{
+public class HomeWork13 extends BaseHomeWork {
     @Test
     @DisplayName("Проверка успешной авторизации пользователя")
-    void test1(){
-        fillInputField(By.cssSelector("[name='Email']"),"larysa@gmail.com");
-        fillInputField(By.cssSelector("[name='Password']"),"qwerty");
+    void test1() {
+        fillInputField(By.cssSelector("[name='Email']"), "larysa@gmail.com");
+        fillInputField(By.cssSelector("[name='Password']"), "qwerty");
         clickOnElement(By.cssSelector("[value='Log in']"));
         checkElementOnDisplay(By.cssSelector("[href='/logout']"));
     }
