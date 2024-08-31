8875c0c2b8a0c321d2d98b25338542723e4bc7b7
larysa7408 <larisa7408tema@gmail.com>
2024-08-31 16:46:47 +0200

    logic was corrected and click on Log out was added

--- Selenium_First_Project/src/test/java/homework/BaseHomeWork.java
+++ Selenium_First_Project/src/test/java/homework/BaseHomeWork.java
@@ -17,27 +17,32 @@ import java.time.Duration;
 public class BaseHomeWork {
     protected WebDriver driver;
     protected WebDriverWait wait;
+
     @BeforeEach
-    public void startDriver(){
+    public void startDriver() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-search-engine-choice-screen");
         driver = new ChromeDriver(options);
-        wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://demowebshop.tricentis.com/login");
         Assertions.assertEquals("Demo Web Shop. Login", driver.getTitle(), "Открыта страница с другим Title");
     }
-    public WebElement getElement(By locator){
+
+    public WebElement getElement(By locator) {
         return wait.until(ExpectedConditions.visibilityOf(driver.findElement(locator)));
     }
+
     public WebElement waitForClickableElement(By locator) {
         return wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
+
     public Alert getAlert() {
         return wait.until(ExpectedConditions.alertIsPresent());
     }
+
     public void fillInputField(By locator, String value) {
         WebElement element = waitForClickableElement(locator);
         element.click();
@@ -45,14 +50,17 @@ public class BaseHomeWork {
         element.sendKeys(value);
         Assertions.assertEquals(value, element.getAttribute("value"), "Введенные данные отличаются от данных у элемента");
     }
+
     public void clickOnElement(By locator) {
         waitForClickableElement(locator).click();
     }
+
     public void checkElementOnDisplay(By locator) {
-        Assertions.assertTrue(driver.findElements(locator).size() > 0,String.format("Элемент с локатором - %s не найден", locator));
+        Assertions.assertTrue(driver.findElements(locator).size() > 0, String.format("Элемент с локатором - %s не найден", locator));
     }
+
     @AfterEach
-    public  void closeDriver(){
+    public void closeDriver() {
         driver.quit();
     }
 }
