0f1f771045e28b32135a33361a7b3820d516358c
larysa7408 <larisa7408tema@gmail.com>
2024-09-04 00:16:52 +0200

    Create a separate DemoWebShop module in the project to automate the page. Moved registration and authorization tests to CreateAccountTests and LoginTests. Created the AddItemToCartTests class and tested adding a product to the cart

---
+++ DemoWebShop_v1/src/test/java/test/LoginTests.java
@@ -0,0 +1,21 @@
+package test;
+
+import core.BaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class LoginTests extends BaseTest {
+    @Test
+    @Tag("@SMOKE")
+    @Tag("02")
+    @DisplayName("Проверка успешной авторизации пользователя")
+    void test2() {
+        fillInputField(By.cssSelector("[name='Email']"), TEST_REGISTRATION.getEmail());
+        fillInputField(By.cssSelector("[name='Password']"), TEST_REGISTRATION.getPassword());
+        clickOnElement(By.cssSelector("[value='Log in']"));
+        Assertions.assertTrue(elementIsDisplayed(By.cssSelector("[href='/logout']")));
+    }
+}
