848e45c43576cc9e45947896fc34261ec5ee561c
larysa7408 <larisa7408tema@gmail.com>
2024-10-01 22:09:35 +0200

    Added a new JQueryUIMenuPage
    Test of three silder on the page.

--- HomeWork/ElementalSelenium_POM_v3/src/main/java/pages/JQueryUIMenuPage.java
+++ HomeWork/ElementalSelenium_POM_v3/src/main/java/pages/JQueryUIMenuPage.java
@@ -9,42 +9,27 @@ import java.util.List;
 
 public class JQueryUIMenuPage extends BasePage {
     @FindBy(id = "menu")
-    private WebElement menuLink;
+    public WebElement menuLink;
     @FindBy(id = "ui-id-1")
-    private WebElement disabletFieled;
+    public WebElement disabletFieled;
     @FindBy(id = "ui-id-3")
-    private WebElement enabledFieled;
+    public WebElement enabledFieled;
     @FindBy(id = "ui-id-4")
-    private WebElement downloadsFieled;
+    public WebElement downloadsFieled;
+    @FindBy(xpath = "//*[text() = 'PDF']")
+    public WebElement pdfMenu;
     @FindBy(tagName = "a")
     List<WebElement> links;
 
-    public WebElement getMenuLink() {
-        return menuLink;
-    }
-
-    public WebElement getDisabletFieled() {
-        return disabletFieled;
-    }
-
-    public WebElement getEnabledFieled() {
-        return enabledFieled;
-    }
-
-    public WebElement getDownloadsFieled() {
-        return downloadsFieled;
-    }
-
-
     public JQueryUIMenuPage checkAllLinks() {
-        System.out.printf("Общее количество ссылок - %s\n", links.size());
+        System.out.printf("Общее количество ссылок - %s", links.size());
         for (WebElement link : links) {
             String textLink = link.getText();
             String linkUrl = link.getAttribute("href");
             if (linkUrl != null && !linkUrl.contains("javascript:void")) {
                 Assertions.assertTrue(linkIsValid(linkUrl));
             } else {
-                System.err.printf("Ссылка - %s, с текстом - %s, вовсе не ссылка - BROKEN\n", linkUrl, textLink);
+                System.err.printf("Ссылка - %s, с текстом - %s, вовсе не ссылка - BROKEN", linkUrl, textLink);
             }
         }
         return this;
--- HomeWork/ElementalSelenium_POM_v3/src/test/java/JQueryUIMenuTest.java
+++ HomeWork/ElementalSelenium_POM_v3/src/test/java/JQueryUIMenuTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static core.AppManager.homePages;
 import static core.AppManager.jQueryUIMenuPage;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JQueryUIMenuTest extends BaseTest {
     @BeforeAll
@@ -21,26 +20,9 @@ public class JQueryUIMenuTest extends BaseTest {
     @Test
     void test1() {
         jQueryUIMenuPage.checkAllLinks();
-    }
-
-    @Test
-    void test2() {
-        assertTrue(jQueryUIMenuPage.getDisabletFieled().isDisplayed(), "Поле 'Disabled' не отображается");
-        assertTrue(!jQueryUIMenuPage.getDisabletFieled().isEnabled(), "Поле 'Disabled' должно быть неактивным");
-
-    }
-
-    @Test
-    void test3() {
-        assertTrue(jQueryUIMenuPage.getEnabledFieled().isDisplayed(), "Поле 'Enabled' не отображается");
-        assertTrue(jQueryUIMenuPage.getEnabledFieled().isEnabled(), "Поле 'Enabled' не активно");
-        jQueryUIMenuPage.getEnabledFieled().click();
-    }
-
-    @Test
-    void test4() {
-        assertTrue(jQueryUIMenuPage.getDownloadsFieled().isDisplayed(), "Поле 'Downloads' не отображается");
-        assertTrue(jQueryUIMenuPage.getDownloadsFieled().isEnabled(), "Поле 'Downloads' не активно");
-        jQueryUIMenuPage.getDownloadsFieled().click();
+        jQueryUIMenuPage.enabledFieled.click();
+        jQueryUIMenuPage.downloadsFieled.click();
+        jQueryUIMenuPage.pdfMenu.click();
+        System.out.println("PDF страница загружена");
     }
 }
